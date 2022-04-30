d8502b3e8eaddc31c59a8b93e5076e873ca17a38
abcdhhhh <1259232949@qq.com>
2022-04-30 13:43:51 +0800

    change dataset

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ from Apriori import Apriori
 from FPGrowth import FPGrowth
 from Eclat import Eclat
 import time
-import random
+# import random
 
 
 def load_data():
-    # dataset = [[1, 2, 5], [2, 4], [2, 3], [1, 2, 4], [1, 3], [2, 3], [1, 3], [1, 2, 3, 5], [1, 2, 3]]
+    dataset = [[1, 2, 5], [2, 4], [2, 3], [1, 2, 4], [1, 3], [2, 3], [1, 3], [1, 2, 3, 5], [1, 2, 3]]
     # dataset = [['Milk', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'], ['Dill', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'], ['Milk', 'Apple', 'Kidney Beans', 'Eggs'],
     #            ['Milk', 'Unicorn', 'Corn', 'Kidney Beans', 'Yogurt'], ['Corn', 'Onion', 'Onion', 'Kidney Beans', 'Ice cream', 'Eggs']]
     # dataset = [['r', 'z', 'h', 'j', 'p'], ['z', 'y', 'x', 'w', 'v', 'u', 't', 's'], ['z', 'p', 'x'], ['r', 'x', 'n', 'o', 's'], ['y', 'r', 'x', 'z', 'q', 't', 'p'],
     #            ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']]
     # dataset = [[0], [0, 1], [0, 1]]
-    dataset = []
-    for i in range(100):
-        tmp = []
-        for j in range(100):
-            if random.random() < 0.1:
-                tmp.append(j)
-        dataset.append(tmp)
+    # dataset = []
+    # for i in range(100):
+    #     tmp = []
+    #     for j in range(100):
+    #         if random.random() < 0.1:
+    #             tmp.append(j)
+    #     dataset.append(tmp)
     return dataset
 
 
@@ -31,6 +31,6 @@ if __name__ == '__main__':
         print(method.__name__)
         start = time.time()
         L = method(dataset=dataset, min_sup=5)
-        # print(L)
+        print(L)
         # print(len(L))
         print("time cost:", time.time() - start)
