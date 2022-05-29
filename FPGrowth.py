f91b432024cf7d82eca50892ef636c5c5d3a23f8
abcdhhhh <60809807+abcdhhhh@users.noreply.github.com>
2022-05-29 10:58:47 +0800

    Update FPGrowth.py

--- FPGrowth.py
+++ FPGrowth.py
@@ -10,7 +10,7 @@ class treeNode:
         self.count += numOccur
 
     def disp(self, dep=1):
-        print((dep, self.name, ' ', self.count))
+        print(dep, ' ' * dep, self.name, self.count)
         for child in self.children.values():
             child.disp(dep + 1)
 
@@ -18,7 +18,7 @@ class treeNode:
     # return self.count < other.count
 
 
-def createTree(dataSet, min_sup=1):  # create FP-tree from dataset but don't mine
+def createTree(dataSet: dict, min_sup=1):  # create FP-tree from dataset but don't mine
     headerTable = {}
     # go over dataSet twice
     for trans in dataSet:  # first pass counts frequency of occurance
@@ -28,37 +28,39 @@ def createTree(dataSet, min_sup=1):  # create FP-tree from dataset but don't min
     for k in list(headerTable.keys()):  # remove items not meeting min_sup
         if headerTable[k] < min_sup:
             headerTable.pop(k)
-    freqItemSet = set(headerTable.keys())
+    # print('headerTable:', headerTable)
+    bigL = [k for k, v in sorted(headerTable.items(), key=lambda p: p[1], reverse=True)]
+    # print('bigL:', bigL)
 
-    # print 'freqItemSet: ',freqItemSet
-    if len(freqItemSet) == 0:
-        return None, None  # if no items meet min support -->get out
+    # print('freqItemSet: ',freqItemSet)
+    if len(bigL) == 0:
+        return None, None, None  # if no items meet min support -->get out
     for k in headerTable:
         headerTable[k] = [headerTable[k], None]  # reformat headerTable to use Node link
     # print 'headerTable: ',headerTable
-    retTree = treeNode('Null Set', 1, None)  # create tree
+    retTree = treeNode('root', 1, None)  # create tree
     for tranSet, count in dataSet.items():  # go through dataset 2nd time
-        localD = {}
-        for item in tranSet:  # put transaction items in order
-            if item in freqItemSet:
-                localD[item] = headerTable[item][0]
-        if len(localD) > 0:
-            orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
+        orderedItems = []
+        for item in bigL:
+            if item in tranSet:
+                orderedItems.append(item)
+        if len(orderedItems) > 0:
+            # print(tranSet, count, orderedItems, sep='\t')
             updateTree(orderedItems, retTree, headerTable, count)  # populate tree with ordered freq itemset
-    return retTree, headerTable  # return tree and header table
+    return retTree, headerTable, bigL  # return tree and header table
 
 
 def updateTree(items, inTree, headerTable, count):
-    if items[0] in inTree.children:  # check if orderedItems[0] in retTree.children
-        inTree.children[items[0]].inc(count)  # incrament count
-    else:  # add items[0] to inTree.children
-        inTree.children[items[0]] = treeNode(items[0], count, inTree)
-        if headerTable[items[0]][1] is None:  # update header table
-            headerTable[items[0]][1] = inTree.children[items[0]]
-        else:
-            updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
-    if len(items) > 1:  # call updateTree() with remaining ordered items
-        updateTree(items[1::], inTree.children[items[0]], headerTable, count)
+    for item in items:
+        if item in inTree.children:  # check if orderedItems[i] in children
+            inTree.children[item].inc(count)  # incrament count
+        else:  # add item to inTree.children
+            inTree.children[item] = treeNode(item, count, inTree)
+            if headerTable[item][1] is None:  # update header table
+                headerTable[item][1] = inTree.children[item]
+            else:
+                updateHeader(headerTable[item][1], inTree.children[item])
+        inTree = inTree.children[item]
 
 
 def updateHeader(nodeToTest, targetNode):  # this version does not use recursion
@@ -68,38 +70,39 @@ def updateHeader(nodeToTest, targetNode):  # this version does not use recursion
 
 
 def ascendTree(leafNode, prefixPath):  # ascends from leaf node to root
-    if leafNode.parent is not None:
+    while (leafNode.parent is not None):
         prefixPath.append(leafNode.name)
-        ascendTree(leafNode.parent, prefixPath)
+        leafNode = leafNode.parent
 
 
-def findPrefixPath(basePat, treeNode):  # treeNode comes from header table
+def findPrefixPath(basePat, node):  # node comes from header table
+    assert (node.name == basePat)
     condPats = {}
-    while treeNode is not None:
+    while node is not None:
         prefixPath = []
-        ascendTree(treeNode, prefixPath)
+        ascendTree(node, prefixPath)
+        assert (prefixPath[0] == basePat)
         if len(prefixPath) > 1:
-            condPats[frozenset(prefixPath[1:])] = treeNode.count
-        treeNode = treeNode.nodeLink
+            condPats[frozenset(prefixPath[1:])] = node.count
+        node = node.nodeLink
     return condPats
 
 
-def mineTree(inTree, headerTable, min_sup, preFix, freqItemList):
-    bigL = [k for k, v in sorted(headerTable.items(), key=lambda p: p[1][0])]  # (sort header table)
-    for basePat in bigL:  # start from bottom of header table
+def mineTree(inTree, headerTable, bigL, min_sup, preFix, freqItemList):
+    for basePat in bigL[::-1]:  # start from bottom of header table
         newFreqSet = preFix.copy()
         newFreqSet.add(basePat)
-        # print 'finalFrequent Item: ',newFreqSet    # append to set
+        # print('finalFrequent Item: ', newFreqSet)  # append to set
         freqItemList.append(newFreqSet)
         condPattBases = findPrefixPath(basePat, headerTable[basePat][1])
-        # print 'condPattBases :',basePat, condPattBases
+        # print('condPattBases :',basePat, condPattBases)
         # 2. construct cond FP-tree from cond. pattern base
-        myCondTree, myHead = createTree(condPattBases, min_sup)
+        myCondTree, myHead, myBigL = createTree(condPattBases, min_sup)
         # print 'head from conditional tree: ', myHead
         if myHead is not None:  # 3. mine cond. FP-tree
             # print 'conditional tree for: ',newFreqSet
             # myCondTree.disp(1)
-            mineTree(myCondTree, myHead, min_sup, newFreqSet, freqItemList)  # 递归
+            mineTree(myCondTree, myHead, myBigL, min_sup, newFreqSet, freqItemList)  # 递归
 
 
 def createInitSet(dataSet):
@@ -109,18 +112,12 @@ def createInitSet(dataSet):
     return retDict
 
 
-def FPGrowth(dataset, min_sup):
-    initSet = createInitSet(dataset)
-    myFPtree, myHeaderTab = createTree(initSet, min_sup)
+def FPGrowth(dataSet, min_sup):
+    initSet = createInitSet(dataSet)
+    # print('initSet:', initSet)
+    myFPtree, myHeaderTab, bigL = createTree(initSet, min_sup)
     myFreqList = []
     if myFPtree is not None:
         # myFPtree.disp()
-        mineTree(myFPtree, myHeaderTab, min_sup, set(), myFreqList)
+        mineTree(myFPtree, myHeaderTab, bigL, min_sup, set(), myFreqList)
     return myFreqList
-
-
-if __name__ == '__main__':
-    dataset = [['r', 'z', 'h', 'j', 'p'], ['z', 'y', 'x', 'w', 'v', 'u', 't', 's'], ['z', 'p', 'x'], ['r', 'x', 'n', 'o', 's'], ['y', 'r', 'x', 'z', 'q', 't', 'p'],
-               ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']]
-    L = FPGrowth(dataset, min_sup=3)
-    print(L)
